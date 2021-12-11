d4ede9ad1a49c6a8d13462401c06e3fcb4bfeec6
Jeffrey <Jeffr@DESKTOP-9IBKN8H>
2021-12-11 09:40:30 -0500

    wip day1_2

--- AdventOfCode/2021/Main.cs
+++ AdventOfCode/2021/Main.cs
@@ -11,8 +11,8 @@ namespace AdventOfCode._2021
             //https://adventofcode.com/2021/day/1
             int previousDepth = -1;
             int countUp = 0;
-            List<string> lines = System.IO.File.ReadAllLines(@"2021/Day1_1.txt").ToList();
-            
+            List<string> lines = System.IO.File.ReadAllLines(@"2021/Day1.txt").ToList();
+
             foreach (var depthString in lines)
             {
                 int depth = Convert.ToInt32(depthString);
@@ -35,34 +35,56 @@ namespace AdventOfCode._2021
                 }
                 previousDepth = depth;
             }
-            
+
             return countUp;
         }
 
         public int Day1_2()
         {
-            List<string> lines = System.IO.File.ReadAllLines(@"2021/Day1_1.txt").ToList();
+            List<string> lines = System.IO.File.ReadAllLines(@"2021/Day1.txt").ToList();
             List<int> lastThree = new List<int>();
-            int lastMeasurement = 0;
-            for (int i = 0; i <= lines.Count; i++)
+            int lastMeasurement = -1;
+            int count = 0;
+            for (int i = 0; i < lines.Count; i++)
             {
                 int depth = Convert.ToInt32(lines[i]);
                 if (lastThree.Count < 3)
                 {
                     lastThree.Add(depth);
-                    if (lastThree.Count == 3)
-                    {
-                        foreach (var depthValue in lastThree)
-                        {
-                            lastMeasurement += depthValue;
-                        }
+                }
 
+                if (lastThree.Count == 3)
+                {
+                    int currentMeasurement = lastThree.Sum();
+                    if (lastMeasurement == -1)
+                    {
+                        lastMeasurement = currentMeasurement;
+                        lastThree.RemoveAt(0);
+                        Console.WriteLine("No previous");
+                        continue;
+                    }
+                    else
+                    {
+                        if(currentMeasurement == lastMeasurement)
+                        {
+                            Console.WriteLine("No Change");
+                        }
+                        else if (currentMeasurement > lastMeasurement)
+                        {
+                            count++;
+                            lastMeasurement = currentMeasurement;
+                            Console.WriteLine("Up");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Down");
+                        }
                         lastThree.RemoveAt(0);
                     }
                 }
             }
 
-            return 0; 
+            return count;
         }
     }
 }
