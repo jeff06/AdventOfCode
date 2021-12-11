16f43602a9deed3a30aada57e03eb371e129a6b0
ImReallyJeff <jeffreyd@live.fr>
2021-12-10 23:04:58 -0500

    Add project files.

---
+++ AdventOfCode/2021/Main.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode._2021
+{
+    public class Event2021
+    {
+        public int Day1_1()
+        {
+            //https://adventofcode.com/2021/day/1
+            int previousDepth = -1;
+            int countUp = 0;
+            List<string> lines = System.IO.File.ReadAllLines(@"2021/Day1_1.txt").ToList();
+            
+            foreach (var depthString in lines)
+            {
+                int depth = Convert.ToInt32(depthString);
+                if (previousDepth == -1)
+                {
+                    Console.WriteLine("No previous measurement");
+                }
+                else if (depth == previousDepth)
+                {
+                    Console.WriteLine("Same as the last");
+                }
+                else if (depth > previousDepth)
+                {
+                    Console.WriteLine("Up");
+                    countUp++;
+                }
+                else
+                {
+                    Console.WriteLine("Down");
+                }
+                previousDepth = depth;
+            }
+            
+            return countUp;
+        }
+
+        public int Day1_2()
+        {
+            List<string> lines = System.IO.File.ReadAllLines(@"2021/Day1_1.txt").ToList();
+            List<int> lastThree = new List<int>();
+            int lastMeasurement = 0;
+            for (int i = 0; i <= lines.Count; i++)
+            {
+                int depth = Convert.ToInt32(lines[i]);
+                if (lastThree.Count < 3)
+                {
+                    lastThree.Add(depth);
+                    if (lastThree.Count == 3)
+                    {
+                        foreach (var depthValue in lastThree)
+                        {
+                            lastMeasurement += depthValue;
+                        }
+
+                        lastThree.RemoveAt(0);
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AdventOfCode._2021;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdventOfCode._2021.Event2021 Event2021 = new Event2021();
+            Console.WriteLine(Event2021.Day1_2());
+        }
+    }
+}
